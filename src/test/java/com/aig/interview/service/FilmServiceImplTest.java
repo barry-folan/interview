11e3a5e140dad1b3b0bf5e7a8729a7c5bc238101
bfolan <Barry.Folan@aig.com>
2018-11-01 11:21:56 +0000

    Reduced to one simple service and repo. Added unit test classes

--- src/test/java/com/aig/interview/service/FilmServiceImplTest.java
+++ src/test/java/com/aig/interview/service/FilmServiceImplTest.java
@@ -1,11 +1,8 @@
 package com.aig.interview.service;
 
-import com.aig.interview.repo.FilmRepo;
 import com.aig.interview.repo.entity.Film;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 // complete this test class
@@ -15,10 +12,6 @@ public class FilmServiceImplTest {
   String existingFilmName = "The Commitments";
   String nonExistingFilmName = "xyz";
 
-  @Mock
-  FilmRepo filmRepo;
-
-  @InjectMocks
   private FilmServiceImpl filmServiceImpl;
 
   @Test
